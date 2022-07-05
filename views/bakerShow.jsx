89e95f5dc167e8344a5c1b5d594eac8e9711fd45
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-05 08:21:11 -0700

    cleaning stylsheet

--- views/bakerShow.jsx
+++ views/bakerShow.jsx
@@ -4,11 +4,13 @@ const Default = require('./default')
 function Show ({baker}) {
     return (
       <Default>
-          <h3>{baker.name}</h3>
+        <div className="wrapper baker-container">
+          <h2>{baker.name}</h2>
           <p>{baker.name} has been baking with us since {baker.startDate}</p>
           <p>About {baker.name}: {baker.bio}</p>
+          <br />
           <h3>Breads {baker.name} has baked</h3>
-          <ul>
+          <ul className='color-highlight'>
             {
               baker.breads.map((bread) => {
                 return(
@@ -22,6 +24,7 @@ function Show ({baker}) {
           {/* <form action={`/bakers/${baker.id}?_method=DELETE`} method="POST">
             <input type="submit" value="DELETE"/>
           </form> */}
+        </div>
       </Default>
     )
 }
