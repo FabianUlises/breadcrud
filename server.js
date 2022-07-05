739819d3b706d0bcd0c3ad5933a6a32e22fd5ef6
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-05 03:31:53 -0700

    Removed console logs

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const mongoose = require('mongoose');
 // Connecting to mongoose and DB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) 
-    }
+    useUnifiedTopology: true}
 )
 
 // Server Listening
-app.listen(process.env.PORT, () => {
-    console.log('I am awake!');
-});
+app.listen(process.env.PORT);
