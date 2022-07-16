d78510353b5b16c8c4dc5702810587258cf55fe8
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-16 05:12:37 -0700

    Refactored all routes with a function that catches errors

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -1,39 +1,30 @@
 const Baker = require('../models/baker');
 const bakerSeedData = require('../models/baker_seed.js');
+const catchAsync = require('./../utils/catchAsync');
 
 // Controllers
-exports.getBakers = async (req, res) => {
-    try {
-        const bakers = await Baker.find()
-            .populate('breads');
+exports.getBakers = catchAsync(async (req, res, next) => {
+    const bakers = await Baker.find().populate('breads');
+    res.json(bakers);
+});
 
-        res.json(bakers)
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-    }
-};
+exports.getBaker =  catchAsync(async (req, res, next) => {
+    const baker = await Baker.findById(req.params.id)
+        .populate({
+            path: 'breads',
+            options: { limit: 2 }
+        });
+    res.render('bakerShow', {
+        baker: baker
+    });
+});
 
-exports.getBaker =  async (req, res) => {
-    try {
-        const baker = await Baker.findById(req.params.id)
-            .populate({
-                path: 'breads',
-                options: { limit: 2 }
-            })
-            
-        res.render('bakerShow', {
-            baker: baker
-        })
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-    }
-};
+
+exports.deleteBaker =  catchAsync(async (req, res, next) => {
+    await Baker.findByIdAndDelete(req.params.id);
+    res.status(303).redirect('/breads');
+});
+
 
 exports.seedData = async (req, res) => {
     try {
@@ -41,18 +32,6 @@ exports.seedData = async (req, res) => {
         (res.redirect('/breads'));
     } catch (err) {
         res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-    }
-};
-
-exports.deleteBaker =  async (req, res) => {
-    try {
-        await Baker.findByIdAndDelete(req.params.id) ;
-        res.status(303).redirect('/breads')
-    } catch (err) {
-        res.status(400).json({
             status: 'fail',
             message: err
         })
--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,8 +3,7 @@ const Baker = require('../models/baker');
 const catchAsync = require('./../utils/catchAsync');
 
 // Controllers
-exports.getAllBreadsShow = catchAsync ( async(req, res, next) => {
-
+exports.getAllBreadsShow = catchAsync(async(req, res, next) => {
     const foundBakers = await Baker.find().lean();
     const foundBreads = await Bread.find().limit(2).lean();
     res.status(200).render('index', {
@@ -14,23 +13,15 @@ exports.getAllBreadsShow = catchAsync ( async(req, res, next) => {
     });
 });
     
-exports.getBreadShow = async (req, res) => {
-  try {
-    const bread = await Bread.findById(req.params.id)
-      .populate('baker');
+exports.getBreadShow = catchAsync(async (req, res, next) => {
+    const bread = await Bread.findById(req.params.id).populate('baker');
     res.render('show', {
       bread: bread
-    })
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    }) 
-  }
-};
+    });
+});
 
 
-exports.createBread = catchAsync( async (req, res, next) => {
+exports.createBread = catchAsync(async (req, res, next) => {
   if (!req.body.image) {
     req.body.image = undefined
   }
@@ -43,62 +34,34 @@ exports.createBread = catchAsync( async (req, res, next) => {
   res.redirect('/breads')
 });
 
-exports.createBreadShow = async (req, res) => {
-  try {
+exports.createBreadShow = catchAsync(async (req, res, next) => {
     const bakers = await Baker.find();
     res.render('new', {
       bakers: bakers
     });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    })
-  }
-};
+});
 
 
-exports.updateBreadShow = async (req, res) => {
-  try {
+exports.updateBreadShow = catchAsync(async (req, res, next) => {
     const bread = await Bread.findById(req.params.id);
     const bakers = await Baker.find();
     res.render('edit', {
       bread: bread,
       bakers: bakers
-    })
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    })
-  }
-};
-
-exports.updateBread = async (req, res) => {
-  try {
-    if(req.body.hasGluten === 'on'){
-      req.body.hasGluten = true
-    } else {
-      req.body.hasGluten = false
-    }
-    await Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    res.redirect(`/breads/${req.params.id}`);
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      messagE: 'Invalid data sent!'
     });
-  }
-};
+});
 
-exports.deleteBread = async (req, res) => {
-  try {
-    await Bread.findByIdAndDelete(req.params.id)
-    res.status(303).redirect('/breads');
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err
-    })
+exports.updateBread = catchAsync(async (req, res, next) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
   }
-};
+  await Bread.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.redirect(`/breads/${req.params.id}`);
+});
+
+exports.deleteBread = catchAsync(async (req, res, next) => {
+  await Bread.findByIdAndDelete(req.params.id)
+  res.status(303).redirect('/breads');
+});
