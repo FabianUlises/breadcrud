69bdc246a75a3ba68e347149e8200f4c2e7fc8d8
FabianUlises <ulisesfab.car@outlook.com>
2023-05-04 04:41:47 -0700

    Added comments to bakers controller

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -1,19 +1,24 @@
+// Model
 const Baker = require('../models/baker');
+// Seeder
 const bakerSeedData = require('../models/baker_seed.js');
-
 // Controllers
+// Get all bakers
 exports.getBakers = async (req, res) => {
     try {
+        // Getting bakers from db with breads
         const bakers = await Baker.find().populate('breads');
+        // Sending back data
         res.json(bakers);
     }
     catch (err) {
         res.status(404).render('error');
     }
 };
-
+// Get single baker
 exports.getBaker = async (req, res) => {
     try {
+        // Getting baker from db using params id including breads
         const baker = await Baker.findById(req.params.id)
             .populate({
                 path: 'breads',
@@ -27,17 +32,19 @@ exports.getBaker = async (req, res) => {
         res.status(404).render('error');
     }
 };
-
-
+// Delete single baker
 exports.deleteBaker = async (req, res) => {
+    // Deleting baker from db
     await Baker.findByIdAndDelete(req.params.id);
+    // Redirecting to /breads
     res.status(303).redirect('/breads');
 };
-
-
+// Seed data controller
 exports.seedData = async (req, res) => {
     try {
+        // Inserting data to db
         await Baker.insertMany(bakerSeedData);
+        // Redirecting to /breads
         (res.redirect('/breads'));
     } catch (err) {
         res.status(404).json({
